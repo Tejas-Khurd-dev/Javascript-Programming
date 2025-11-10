509382dd0acb77ea7d701c5963ebbad3eab12451
Tejas-Khurd-dev <tejaskhurd.dev@gmail.com>
2025-11-10 22:35:11 +0530

    added javascript codes

--- Intro/varLetConst.js
+++ Intro/varLetConst.js
@@ -1,3 +1,29 @@
+/* 
+varf is Function-scoped, not globally scoped by default.
+If you declare var inside a function, it is scoped to that function.
+If declared outside any function, then it becomes global.
+var is also hoisted and initialized with undefined. --> meaning
+{
+console.log(a)
+var a = 10 
+}
+so above code output is undefined
+It does not respect block scope (e.g., inside if, for, { }). 
+
+
+
+
+let is Block-scoped — limited to { }.
+Hoisted but not initialized, so accessing before declaration gives a ReferenceError.
+--> meaning
+{
+console.log(a)
+let a = 10 
+}
+so above code output gives a ReferenceError
+Safer for modern JavaScript.
+*/
+
 var a = 4
 console.log(a)
 a = 5
@@ -12,7 +38,7 @@ console.log(b)
 console.log("--------------------------------\n")
 // ----------------------------------------------------------------------------------------------------------------------
 
-var a = 8
+let a = 8
 console.log(a)
 a = 7
 console.log(a)

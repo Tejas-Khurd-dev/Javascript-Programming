b4b5b6e4047cb55760feb230878827dda0ccc9c6
Tejas-Khurd-dev <tejaskhurd.dev@gmail.com>
2025-11-10 19:48:17 +0530

    added javascript codes

---
+++ Intro/dataTypes.js
@@ -0,0 +1,13 @@
+a = 4
+b = 3.14
+
+c = 'a'
+d = 'string'
+
+e = true
+
+console.log(a, 'is', typeof a , "\n")
+console.log(b, 'is', typeof b , "\n")
+console.log(c, 'is', typeof(c), "\n")
+console.log(d, 'is', typeof(d), "\n") 
+console.log(e, 'is', typeof(e), "\n")
---
+++ Intro/operators.js
@@ -0,0 +1,19 @@
+let a = 7
+let b = 8
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b) // greater no. % smaller no. = smaller no.
+console.log(Math.floor(a / b))
+
+console.log(10**3) // 10^3 = 1000
+
+
+b++
+a--
+
+++b
+--b
+

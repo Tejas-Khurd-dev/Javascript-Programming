6a2f45b07b5d6f1c7b5682a11eb94894909c8c3a
Tejas-Khurd-dev <tejaskhurd.dev@gmail.com>
2025-11-10 20:19:01 +0530

    added javascript codes

---
+++ Condition Statments/TripleEqualTo.js
@@ -0,0 +1,5 @@
+a = 7
+b = '7'
+
+console.log(a==b) 
+console.log(a===b) 
---
+++ Condition Statments/ternaryOperator.js
@@ -0,0 +1,7 @@
+// condition ? expressionIfTrue : expressionIfFalse
+
+age = 20
+age > 17 ? console.log("Adult") : console.log("Child")
+
+let checkStatus = age > 17 ? "Adult" : "Child"
+console.log(checkStatus)

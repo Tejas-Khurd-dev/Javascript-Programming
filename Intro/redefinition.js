939eed8ba11371c08c92c1d667379bf1727244e2
Tejas-Khurd-dev <tejaskhurd.dev@gmail.com>
2025-11-10 19:22:13 +0530

    added javascript codes

---
+++ Intro/redefinition.js
@@ -0,0 +1,16 @@
+var a = 4
+console.log(a)
+
+var a = 6
+console.log(a)
+
+// const a = 6 // --> this will give error
+console.log(a)
+console.log("--------------------------------\n")
+// ----------------------------------------------------------------------------------------------------------------------
+
+const b = 4
+console.log(a)
+
+// const b = 6 // --> this will give error
+console.log(a)
